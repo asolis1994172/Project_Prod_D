16614fe4f060c3b0bb9ffa50e5fcefd4a6c1425a
Denis Solis <denis.solis@galileo.edu>
2021-11-20 20:34:43 -0600

    pipeline_func

--- my_preprocessors.py
+++ my_preprocessors.py
@@ -24,3 +24,29 @@ class TremporalVariableTransformer(BaseEstimator, TransformerMixin):
         for feature in self.variables:
             X[feature] = X[self.reference_variable] - X[feature]
         return X
+
+#Clase para transformación de variables categóricas
+
+class Mapper(BaseEstimator, TransformerMixin):
+
+    #Constructor
+    def __init__(self, variables, mappings):
+
+        if not isinstance(variables, list):
+            raise ValueError("Las variables deben estar incluidas en una lista")
+
+        #Campos clase mapper
+        self.variables = variables
+        self.mappings = mappings
+
+
+    # Metodo fit
+    def fit(self, X, y = None):
+        return self
+
+    #Metodo transform
+    def transform(self, X):
+        X = X.copy()
+        for variable in self.variables:
+            X[variable] = X[variable].map(self.mappings)
+        return X
