9a110c8012dbcbc0ab238f992f48178f36814ebd
Denis Solis <denis.solis@galileo.edu>
2021-12-08 21:01:07 -0600

    pipeline

--- pipeline_predict.py
+++ pipeline_predict.py
@@ -5,10 +5,6 @@ import config as cfg
 
 import my_preprocessors as mypp
 
-#Cargando dataset
-#X_test = pd.read_csv('X_text.csv')
-#X_test = X_test[cfg.FEATURES]
-
 salary_price_model = joblib.load('Salary_pipeline.pkl')
 
 def predict(X):
